fffe96100e926ec61522ac363fef3ad197a91819
jacknoone <jack.noone@paymentsense.com>
2020-06-08 19:30:40 +0100

    first commit

---
+++ CoolForTheSummerApi/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoolForTheSummerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetTrue()
+        {
+            return  "lol";
+        }
+    }
+}
